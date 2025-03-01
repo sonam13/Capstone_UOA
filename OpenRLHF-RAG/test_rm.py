29d515aa369ba0874e5440f9d99705dc32a72aba
sht <3151273556@qq.com>
2025-03-01 23:48:39 +0800

    1

---
+++ OpenRLHF-RAG/test_rm.py
@@ -0,0 +1,4 @@
+import requests
+
+url=""
+response = requests.post(url=url, json=data, headers=headers, timeout=360)
---
+++ scripts/wiki_load.sh
@@ -0,0 +1,1 @@
+python /opt/aps/workdir/sht-RAG_RL/train/wiki_server/wiki_server_start.py hotpotqa 5004 &
